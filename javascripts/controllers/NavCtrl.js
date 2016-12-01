1e73218325bb8bc84091f496c0322b7dd18055fb
Jennifer Dutton <jduttondesign@gmail.com>
2016-11-30 19:07:46 -0600

    added navCtrl.js

--- javascripts/controllers/NavCtrl.js
+++ javascripts/controllers/NavCtrl.js
@@ -1,1 +1,18 @@
 "use strict";
+
+app.controller("NavCtrl", function($scope) {
+    $scope.navItems = [
+    {
+    	name: "Logout",
+		url:"#/logout"
+	}, 
+    {
+    	name:"All Items",
+    	url:"#/items/list"
+	}, 
+    {
+    	name:"New Item",
+    	url:"#/items/new"
+	}
+   ];
+});
