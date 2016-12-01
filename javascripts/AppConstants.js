65f80ff5f76b14bc71bab9b1f2dbe26463665284
Helana Nosrat <helanan@gmail.com>
2016-11-30 19:03:02 -0600

    added partials for login view

--- javascripts/AppConstants.js
+++ javascripts/AppConstants.js
@@ -1,7 +1,7 @@
 "use strict";
 
 app.constant("FIREBASE_CONFIG", {
-    apiKey: "AIzaSyDLYTCrr8KDez8swSpi0Hgef3ymv4xSPVI",
+   apiKey: "AIzaSyDLYTCrr8KDez8swSpi0Hgef3ymv4xSPVI",
     authDomain: "pinthis-fe275.firebaseapp.com",
     databaseURL: "https://pinthis-fe275.firebaseio.com",
     storageBucket: "pinthis-fe275.appspot.com",
