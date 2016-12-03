793ef68904db2fb18ff7c3b84b44f1b902c14876
Jennifer Dutton <jduttondesign@gmail.com>
2016-12-02 22:11:01 -0600

    fixed grunt error

--- javascripts/controllers/BoardViewCtrl.js
+++ javascripts/controllers/BoardViewCtrl.js
@@ -3,7 +3,7 @@
 app.controller("BoardViewCtrl", function($scope, $routeParams, BoardFactory){
 	$scope.selectedBoard = {};
 	let boardId = $routeParams.id;
-	console.log("$routeParams", BoardId);
+	//console.log("$routeParams", BoardId);
 
 	BoardFactory.getSingleBoard(boardId).then(function(oneBoard){
 		oneBoard.id=boardId;
