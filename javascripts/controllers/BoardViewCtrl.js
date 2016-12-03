f2a98dbf45cd330de521f4d0f3c68883d5476c26
Helana Nosrat <helanan@gmail.com>
2016-12-02 19:29:59 -0600

    fixed boardcontroller error of undefined, commented out css

--- javascripts/controllers/BoardViewCtrl.js
+++ javascripts/controllers/BoardViewCtrl.js
@@ -1,14 +1,12 @@
 "use strict";
 
 app.controller("BoardViewCtrl", function($scope, $routeParams, BoardFactory){
-	console.log("routeParams", $routeParams);
 	$scope.selectedBoard = {};
-	let BoardId = $routeParams.id;
+	let boardId = $routeParams.id;
 	console.log("$routeParams", BoardId);
 
 	BoardFactory.getSingleBoard(boardId).then(function(oneBoard){
 		oneBoard.id=boardId;
 		$scope.selectedBoard = oneBoard;
-		console.log("oneBoard", oneBoard);
 	});
 });
